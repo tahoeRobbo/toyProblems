542246f0bf31d2e42a2da7c0e11e2df33f9b54b8
tahoeRobbo <rob@snowyplayground.com>
2015-07-04 15:59:34 -0600

    added sameArrayChecker

---
+++ sameArrayChecker.js
@@ -0,0 +1,22 @@
+var sameArrayChecker = function(arr1, arr2) {
+	if(arr1.length !== arr2.length) {
+		return false;
+	}
+	
+	if(arr1.length === arr2.length) {
+		for(var i = 0; i < arr1.length; i++) {
+			if(arr1[i] !== arr2[i]){
+				return false;
+			}
+		}
+		return true;
+	}
+};
+
+var results = [];
+results.push(sameArrayChecker([1,2,3], [4,5,6]));//false
+results.push(sameArrayChecker([1,2,3], [1,2,3]));//true
+results.push(sameArrayChecker([1,2,3], [1,2,3,4]));//false
+results.unshift("key** 1-false, 2-true, 3-false");
+
+console.log(results, results[0]);

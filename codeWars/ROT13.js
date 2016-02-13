f493534831748ecc6953450a382df4736b98f4b3
tahoeRobbo <rob@snowyplayground.com>
2016-02-13 00:42:21 -0800

    first couple weeks in the books. puzzles feb 12

--- codeWars/ROT13.js
+++ codeWars/ROT13.js
@@ -1,49 +1,17 @@
-//How can you tell an extrovert from an introvert at NSA? Va gur ryringbef, gur rkgebireg ybbxf ng gur BGURE thl'f fubrf.
+Description:
 //
-//I found this joke on USENET, but the punchline is scrambled. Maybe you can decipher it? According to Wikipedia, ROT13 (http://en.wikipedia.org/wiki/ROT13) is frequently used to obfuscate jokes on USENET.
+//ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. ROT13 is an example of the Caesar cipher.
+//
+//Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
+//
+//Please note that using "encode" in Python is considered cheating. 
 
-function ROT13 (str){
-	var decrypt = '';
-	
-	for(var i = 0, len = str.length; i < len; i++){
-		if (/[A-Za-z]/g.test(str[i])){
-			(str.charCodeAt(i) < 78 || (96 < str.charCodeAt(i) && str.charCodeAt(i) < 110))	?
-				decrypt += String.fromCharCode(str.charCodeAt(i) + 13) :
-				decrypt += String.fromCharCode(str.charCodeAt(i) - 13);
-		}else {
-			decrypt += str[i];
-		}
-	}
-	return decrypt;
-}
-	
+//Hooray for one liners!
+const rot13 = (str) => 
+  str.split('').map(char => /[a-z]/i.test(char) ? (char.toLowerCase() > 'm' ? String.fromCharCode(char.charCodeAt(0) - 13) : String.fromCharCode(char.charCodeAt(0) + 13)) : char).join('');
 
-ROT13("Va gur ryringbef, gur rkgebireg ybbxf ng gur BGURE thl'f fubrf");
 
-function ROT13 (str) {
-	var decrypt = '';
-	
-	for(var i = 0, len = str.length; i < len; i++){
-		if(/[A-Za-z]g/.test(str[i])){
-			if(str.charCodeAt(i) < 78 || (96 < str.charCodeAt(i) && str.charCodeAt(i) < 110)){
-				decrypt += String.fromCharCode(str.charCodeAt(i) + 13);
-			}
-			else{
-				decrypt += String.fromCharCode(str.charCodeAt(i) - 13);
-			}
-		}
-		else {
-			decrypt += str[i];
-		}
-	}
-	return decrypt;
-}
-
-ROT13("aZzA -_12 nNMm")
-ROT13("Va gur ryringbef, gur rkgebireg ybbxf ng gur BGURE thl'ffubrf");
-
-//cool concise example -- didn't realize I could check < a letter
-
+//OH JUST SO PWETTY//
 function rot13(str) {
   return str.replace(/[a-z]/ig, function(x){
     return String.fromCharCode(x.charCodeAt(0) + (x.toLowerCase() <= 'm' ? 13: -13));
